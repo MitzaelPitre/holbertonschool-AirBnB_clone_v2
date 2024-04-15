c8de8ae57ea70a009c155448c9c60ec6fe5d20ed
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-04-14 22:46:40 -0400

    Coding

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-from flask import Flask, escape
+"""
+Flask module that starts a web application
+"""
+
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Route that displays 'Hello HBNB!' """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ Route that displays 'HBNB' """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    return "C %s" % escape(text.replace("_", " "))
+    """ Route that displays 'C' followed by the value of the text variable """
+    return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    return "Python %s" % escape(text.replace("_", " "))
+    """ Route that displays 'Python' followed by the value of the text variable """
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
