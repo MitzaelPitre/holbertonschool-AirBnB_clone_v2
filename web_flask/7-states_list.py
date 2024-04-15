704b4b08713c7ede4cb6bff9d97255985385643b
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-04-14 23:45:20 -0400

    Coding

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-"""Script to start a Flask web application"""
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
 from models import storage
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+app.config['DEBUG'] = False  # Desactivar la configuración de depuración
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states_list')
-def state_list():
-    states = storage.all('State')
-    return render_template('7-states_list.html', state=states)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
