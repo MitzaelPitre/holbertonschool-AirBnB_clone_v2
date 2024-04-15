7cc0fcfdcd7d56caa67bb533fea84d4cc3193df8
MitzaelPitre <mitzaelpitredejesus@gmail.com>
2024-04-14 22:19:44 -0400

    Coding

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,19 +5,17 @@ Flask module that returns a route at /
 from flask import Flask, escape
 
 app = Flask(__name__)
-
+app.debug = False  # Deshabilitar la depuración
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ A route that displays Hello HBNB! """
     return "Hello HBNB!"
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ A route that displays HBNB """
     return "HBNB"
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
@@ -25,4 +23,5 @@ def c_text(text):
     return "C %s" % escape(text.replace("_", " "))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
+
